362f48458037207687332348f41666afac4c2a6e
Ricardo Chihu <ricardo.chihu@unosquare.com>
2017-10-12 13:01:06 -0500

    put modified to post

--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ Router.get('/', todoController.getAll)
 Router.post('/', todoController.save)
 //Router.delete('/{id}', userController.deleteTrue)
 
-Router.put('/user', userController.newUser)
+Router.post('/user', userController.newUser)
 Router.delete('/user', userController.deleteTrue)
 
 module.exports = Router
